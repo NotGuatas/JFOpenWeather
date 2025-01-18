6f373be0fc822b72bc390ac676f56fad1c69d328
NotGuatas <josephaugustoflores@gmail.com>
2025-01-18 10:13:56 -0500

    Agregar archivos de proyecto.

---
+++ JFOpenWeather/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JFOpenWeather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JFOpenWeather/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JFOpenWeather
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
